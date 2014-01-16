cf1a86ecb3e61cd795c525332ab3bef415396fed
Chris Wright <christopher.wright47@gmail.com>
2014-01-16 10:09:53 -0800

    added a blank homepage

--- phase_zero_pairing/config/routes.rb
+++ phase_zero_pairing/config/routes.rb
@@ -1,4 +1,6 @@
 PhaseZeroPairing::Application.routes.draw do
+
+  get '/' => 'static_pages#home', as: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
