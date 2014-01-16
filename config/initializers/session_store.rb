3df76e190e194b4afb2f3489ecf62dcd92e4b126
Chris Wright <christopher.wright47@gmail.com>
2014-01-16 12:40:49 -0800

    had to restart git tracking

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhaseZeroPairing::Application.config.session_store :cookie_store, key: '_phase_zero_pairing_session'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+PhaseZeroPairing::Application.routes.draw do
+
+  get '/' => 'static_pages#home', as: 'home'
+
+end
