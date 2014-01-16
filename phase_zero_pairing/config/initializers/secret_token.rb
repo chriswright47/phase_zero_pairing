796d77570b471263721a0c976a80317418de3dcb
Chris Wright <christopher.wright47@gmail.com>
2014-01-16 09:46:27 -0800

    initial commit, set up new rails app

---
+++ phase_zero_pairing/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhaseZeroPairing::Application.config.secret_key_base = '9f1999bbab0b00eb359497d2135388843fdc7caf85023cc160c9decefb1fcb326afaf418d17edc3c2252571c45f73e37dd37aa85df619345cbceb2b2489d168d'
