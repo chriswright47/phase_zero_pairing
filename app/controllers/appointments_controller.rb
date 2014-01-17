64127ba2112632dfb5aa1947efe86e49fc9db400
Chris Wright <christopher.wright47@gmail.com>
2014-01-17 13:36:14 -0800

    adding functionality of teacher creating pairing sessions, still need to implement functionality of students signing up

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -13,6 +13,7 @@ class AppointmentsController < ApplicationController
 
   def create
     appointment = Appointment.new(appointment_params)
+    appointment.teacher = current_user
     if appointment.save
       flash[:success] = 'Pairing session successfully created'
       redirect_to appointment_path(appointment)
